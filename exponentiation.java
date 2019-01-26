b0bd28fc6a191bb8a2e1d9f1704aebdae3ed6116
Alexander Zemskov <zaismit@gmail.com>
2019-01-26 13:13:19 +0200

    Add files via upload

---
+++ exponentiation.java
@@ -0,0 +1,7 @@
+public class exponentiation extends main_file
+{
+    int exponent(int i)
+    {
+        return i*i;
+    }
+}
---
+++ main_file.java
@@ -0,0 +1,16 @@
+public class main_file {
+
+    public static void main(String args[]){
+
+        exponentiation ourexponent;
+
+            ourexponent = new exponentiation();
+
+        int expo;
+
+            expo = ourexponent.exponent(5);
+
+            System.out.println(expo);
+    }
+
+}
